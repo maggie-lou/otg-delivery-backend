04c80827eee45e0332583a3d2f2998e6bea071cd
Sam Naser <sammnaser@gmail.com>
2018-05-02 19:33:28 -0500

    >:(

--- controllers/RequestController.js
+++ controllers/RequestController.js
@@ -2,7 +2,7 @@
 var PushController = require('./push');
 
 var express = require('express');
-var mongoose = require('mongoose');
+var ObjectId = require('mongoose').Types.ObjectId
 
 var Request = require('../models/request');
 var User = require('../models/User'); 
@@ -135,7 +135,7 @@ router.route('/accept/:userId')
   .get(function(req, res) {
     console.log("GET: accepted tasks for " + req.params.userId);
 
-    Request.find({ helper: new mongoose.Types.ObjectId(req.params.userId), status: 'Accepted', 'endTime': {$gte: Date.now()}}, function(err, requests) {
+    Request.find({ helper: new ObjectId(req.params.userId), status: 'Accepted', 'endTime': {$gte: Date.now()}}, function(err, requests) {
       if (err) {
         console.log("Error getting accepted tasks for " + req.params.userId);
         res.send(err);
