e75d5bf919214d2aa759423b7a47c3b54bfa942c
Maggie <margaretlou2019@u.northwestern.edu>
2019-03-11 17:34:39 -0500

    Have parsing convert dates to Central timezone

--- controllers/parsing.js
+++ controllers/parsing.js
@@ -4,7 +4,7 @@ module.exports = {
 
   parseDateToTime(dateString) {
     var date = moment(dateString);
-    date = date.format('h:mm a');
+    date = date.utcOffset(-300).format('h:mm a');
     return date;
   }
 
